4410f1d2f70f8568ba0ae0302aed4047a51166a3
coolguy <fansheng0594@users.noreply.github.com>
2020-10-20 13:09:54 +0000

    Apply fixes from StyleCI

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,8 +1,16 @@
 <?php
 
+/*
+ * This file is part of the fan/weather.
+ *
+ * (c) fansheng <fansheng0594@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Fan\Weather\Exceptions;
 
 class HttpException extends Exception
 {
-
 }
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the fan/weather.
+ *
+ * (c) fansheng <fansheng0594@163.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Fan\Weather;
 
 use Illuminate\Contracts\Support\DeferrableProvider;
@@ -15,7 +24,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider implements Def
 
     public function register()
     {
-        $this->app->singleton(Weather::class, function (){
+        $this->app->singleton(Weather::class, function () {
             return new Weather(config('weather.api_key'));
         });
 
